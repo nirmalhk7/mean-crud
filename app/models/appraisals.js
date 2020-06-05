81599f923c915cc95dedd447dbc8ef0b470405b5
Nirmal Khedkar <nirmalhk7@gmail.com>
2020-06-05 19:41:07 +0530

    COmmit
    Signed-off-by: Nirmal Khedkar <nirmalhk7@gmail.com>

--- app/models/appraisals.js
+++ app/models/appraisals.js
@@ -1,7 +1,35 @@
 var mongoose = require('mongoose');
 
+const dummy = require('mongoose-dummy');
+const ignoredFields = ['_id','created_at', '__v', /detail.*_info/];
+
 // define our students model
 // module.exports allows us to pass this to other files when it is called
-module.exports = mongoose.model('Appraisal', {
-   name : {type : String, default: ''}
+var AppraisalSchema = new mongoose.Schema({
+   author : {
+      type : String, 
+      required: true
+   },
+   rating: {
+      type: Number,
+      min: 1,
+      max: 5,
+      required: true
+   },
+   subject: {
+      type: String,
+      required: true
+   },
+   comments: {
+      type: String,
+      default: ''
+   },
+   reviewee: {
+      type : String, 
+      required: true
+   }
 });
+var AppraisalsModel = mongoose.model('Appraisal', AppraisalSchema);
+
+
+module.exports = AppraisalsModel
--- server.js
+++ server.js
@@ -1,87 +1,35 @@
 // modules =================================================
 const express = require('express');
 const app = express();
-const router = express.Router();
+const bodyParser = require('body-parser')
+var createError = require('http-errors');
+const AppraisalRouter = express.Router();
 var mongoose = require('mongoose');
 // set our port
 const port = 3000;
 // configuration ===========================================
 
 // config files
-var db = require('./config/db');
-console.log("connecting--",db);
+mongoose.connect('mongodb://localhost:27017/test',{ useNewUrlParser: true, useUnifiedTopology: true},()=>{
+    console.log("Connecting with Mongoose!")
+})
+.then(msg=>{
+    console.log("Succesful Connection");
+})
+.catch(err=>{
+    console.error("ERROR",err);
+}); //Mongoose connection created
 
-mongoose.connect(db.url,{ useNewUrlParser: true, useUnifiedTopology: true}); //Mongoose connection created
+app.get('/', (req, res) => {
+    res.send('WTA Project');
+    console.log("/",req.method,200)
+    
+});
 
-app.get('/', (req, res) => res.send('WTA Project'));
+app.use('/api/appraisals',require('./routes/appraisal_router'));
+app.use(function(req, res, next) {
+    next(createError(404));
+});
 
-
-var Appraisals = require('./app/models/appraisals');
-
-router.route('/api/appraisals')
-.get((req,res,next)=>{
-    Appraisals.find((err, ans)=> {
-        if (err)
-        {
-            res.send(err);
-            res.statusCode = 403;
-        }
-        res.json(ans); // return all students in JSON format
-    });
-})
-.post((req,res,next)=>{
-    var student = new Appraisals(); // create a new instance of the student model
-    student.name = req.body.name; // set the student name (comes from the request)
-    student.save(function(err) {
-        if (err)
-        {
-            res.send(err);
-            res.statusCode = 403;
-        }
-        res.json({ message: 'successful' });
-    });
-})
-.put((req,res,next)=>{
-    next();
-})
-.delete((req,res,next)=>{
-    Appraisals.remove({})
-    .then((ans) => {
-        res.statusCode = 200;
-        res.json(ans);
-    }, (err) => next(err))
-    .catch((err) => next(err));   
-})
-
-router.route('/api/appraisals/:aprid')
-.get((req,res,next)=>{
-    Appraisals.findById(req.params.aprid)
-    .then((ans) => {
-        res.statusCode = 200;
-        res.json(ans);
-    }, (err) => next(err))
-    .catch((err) => next(err));
-})
-.post((req,res,next)=>{
-    next();
-})
-.put((req,res,next)=>{
-    Appraisals.findByIdAndUpdate(req.params.aprid, {
-        $set: req.body
-    }, { new: true })
-    .then((ans) => {
-        res.statusCode = 200;
-        res.json(ans);
-    }, (err) => next(err))
-    .catch((err) => next(err));
-})
-.delete((req,res,next)=>{
-    Appraisals.findByIdAndRemove(req.params.aprid)
-    .then((resp) => {
-        res.statusCode = 200;
-        res.json(resp);
-    }, (err) => next(err))
-    .catch((err) => next(err));
-})
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port, () => console.log(`Beautiful things happening on ${port}!`));
+//https://www.tutorialspoint.com/meanjs/meanjs_building_single_page_with_angular.htm
